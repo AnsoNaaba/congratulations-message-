9a103d1b1487060d7382b0487bd1059f35087d9c
AnsoNaaba <ansonaaba@gmail.com>
2025-10-24 17:50:47 +0000

    Add files via upload

---
+++ congratulations code.py
@@ -0,0 +1,9 @@
+#birthday message.
+print("What is your name.")
+name = input("enter your name:")
+print ("\n happy birthday", name , "how old are you today")
+age = input("i am :")
+print("years old")
+age = age
+message = f"\n you are {age} years old today!"
+print(message.upper())
